4730ef7c5c95eb248da16eb47d0d7e5aaae69860
Mahardika Yurico Billian <mahardikayurico.my@gmail.com>
2024-10-05 11:38:53 +0700

    Add files via upload

---
+++ data/GunungMerapi_8.js
@@ -0,0 +1,1 @@
+var json_GunungMerapi_8 = {"type":"FeatureCollection","name":"GunungMerapi_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.446204072449262,-7.541274401553396]}}]}
